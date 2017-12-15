4acfcab6b8f3ec41cb09dd188d17d44012c3dada
Coco Matthey <cmatthey@cmatthey-macbookair.roam.corp.google.com>
2017-12-15 11:09:53 -0800

    ran jshint.com, introduce constant

--- frontend-nanodegree-arcade-game/js/app.js
+++ frontend-nanodegree-arcade-game/js/app.js
@@ -1,9 +1,12 @@
+var TILE_WIDTH = 101,
+    TILE_HEIGHT = 83;
+
 var Persona = function(sprite, x, y) {
     this.sprite = sprite;
     Resources.load(this.sprite);
     this.x = x;
     this.y = y;
-}
+};
 
 // Enemies our player must avoid
 var Enemy = function() {
@@ -12,7 +15,7 @@ var Enemy = function() {
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    Persona.call(this, 'images/enemy-bug.png', 0 * 101, (Math.floor(Math.random() * 3) + 1) * 83);
+    Persona.call(this, 'images/enemy-bug.png', 0 * TILE_WIDTH, (Math.floor(Math.random() * 3) + 1) * TILE_HEIGHT);
     this.speed = Math.random();
 };
 
@@ -24,14 +27,14 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x += 101 * this.speed * dt;
+    this.x += TILE_WIDTH * this.speed * dt;
     if (this.x > 505) {
         this.x = 0;
     }
     // console.log("Enemy x, y " + this.x + ", " + this.y);
     // console.log("player x, y " + player.x + ", " + player.y);
-    if ((this.x + 101 >= player.x && this.x <= player.x)
-        && this.y == player.y) {
+    if ((this.x + TILE_WIDTH >= player.x && this.x <= player.x) &&
+        this.y === player.y) {
         console.log("bang");
         player.startSquare();
     }
@@ -47,18 +50,18 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function() {
-    Persona.call(this, 'images/char-pink-girl.png', 2 * 101, 5 * 83);
+    Persona.call(this, 'images/char-pink-girl.png', 2 * TILE_WIDTH, 5 * TILE_HEIGHT);
     this.startSquare();
 };
 
 Player.prototype = Object.create(Persona.prototype);
 
 Player.prototype.update = function() {
-    if (this.y == 0) {
+    if (this.y === 0) {
         console.log("you won");
         this.startSquare();
         allEnemies.forEach(function(enemy){
-            enemy.x = -101;
+            enemy.x = -TILE_WIDTH;
         });
     }
 };
@@ -74,28 +77,28 @@ Player.prototype.handleInput = function(key) {
         case 'left':
             if (this.col > 0) {
                 this.col--;
-                this.x = this.col * 101;
+                this.x = this.col * TILE_WIDTH;
                 this.render();
             }
             break;
         case 'up':
             if (this.row > 0) {
                 this.row--;
-                this.y = this.row * 83;
+                this.y = this.row * TILE_HEIGHT;
                 this.render();
             }
             break;
         case 'right':
             if (this.col < 4) {
                 this.col++;
-                this.x = this.col * 101;
+                this.x = this.col * TILE_WIDTH;
                 this.render();
             }
             break;
         case 'down':
             if (this.row < 5) {
                 this.row++;
-                this.y = this.row * 83;
+                this.y = this.row * TILE_HEIGHT;
                 this.render();
             }
             break;
@@ -107,9 +110,9 @@ Player.prototype.handleInput = function(key) {
 Player.prototype.startSquare = function() {
     this.col = 2;
     this.row = 5;
-    this.x = this.col * 101;
-    this.y = this.row * 83;
-}
+    this.x = this.col * TILE_WIDTH;
+    this.y = this.row * TILE_HEIGHT;
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -118,7 +121,7 @@ var allEnemies = [];
 var numOfEnemies = 3;
 for (var i=0; i<numOfEnemies; i++) {
     allEnemies.push(new Enemy());
-};
+}
 var player = new Player();
 
 // This listens for key presses and sends the keys to your
